d3784c86a789d60dfb485aa516406f7561c5d979
Min Thant <minthantk154@gmail.com>
2024-01-16 22:43:50 +0630

    milestone

---
+++ message/js/index.js
@@ -0,0 +1,22 @@
+this.$dom = {};
+this.$dom.animatedText = document.createElement('div');
+this.text = "Today marks our 2-month milestone, and I just want to say thank you for being with me. Let's keep our special memories close, and no matter what challenges come our way, I'm here with you. I love you so much, and I'll do my best for you.";  // MESSAGE AREA , THIS CAN BE CHANGED FROM YOUR MESSAGE :)
+this.$dom.container = document.getElementById('container');
+this.$dom.container.appendChild(this.$dom.animatedText);
+animateText();
+function animateText(){
+	var arrayOfLetters = this.text.split("");
+	var animatedSpan = [];
+	arrayOfLetters.forEach(function(item){
+		var span = document.createElement('span');
+		span.innerHTML = item;
+		animatedSpan.push(span);
+		span.style.opacity = 0;
+		span.style.color = 'white';
+		this.$dom.animatedText.appendChild(span);
+	});
+	TweenMax.staggerTo(animatedSpan, .2,{
+		opacity : 1,
+		delay : 2
+	},0.05);
+}
